77d92b5a58d7c443320c2f7a106e8d081e1c533f
slightlycyborg <collinalexbell@gmail.com>
2020-04-04 21:07:30 +0000

    added flat cylinder to end of screw

--- src/main/scala/design/Screw.scala
+++ src/main/scala/design/Screw.scala
@@ -9,7 +9,9 @@ import net.joewing.scalacad.io.AwtRenderer
 
 object Screw {
   val threads = Threads(4, 30, 12, 0.75, .075)
+  val cylinder = Cylinder(15, 4, 4, 30)
+  val obj = Union(threads, Translate(cylinder, 0, 0, -15))
   def main(args: Array[String]) = {
-    AwtRenderer.show(threads)
+    AwtRenderer.show(obj)
   }
 }
